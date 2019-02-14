db07f924701970e25244de5fd184f0ac2510876c
csquillz <carolinesquillante@gmail.com>
2019-02-14 15:06:29 -0500

    changes

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,36 @@ const uuid = require("node-uuid");
 const xss = require("xss");
 
 router.get("/", async function (req, res) {
-    res.render("pages/projects");
+    res.render("pages/login", {
+        title: "Login"
+    });
+});
+
+router.post("/login", async function (req, res) {
+    let username = xss(req.body.username);
+    let password = xss(req.body.password);
+
+    const goodUsername = await usersData.checkForExistingUser(username);
+    const goodPassword = await usersData.validatePassword(username, password);
+    if (goodUsername && goodPassword) {
+        res.cookie('name', 'AuthCookie')
+        const sessionId = uuid.v4();
+        res.cookie("sessionId", sessionId)
+        await usersData.addSessionId(sessionId, username)
+        res.redirect("/private");
+    } else {
+        let error = ""
+        if (!goodUsername) {
+            error = "User does not exist"
+        } else {
+            error = "Password is invalid"
+        }
+        res.render("pages/index", {
+            title: "Login",
+            error: error
+        });
+    }
+    return req.cookies.sessionId
 });
 
 router.get("/createProject", async function (req, res) {
