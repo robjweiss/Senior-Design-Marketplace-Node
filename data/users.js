91b53f22baaaa6bf087fce4ca3ea1d41bda06975
csquillz <carolinesquillante@gmail.com>
2019-03-07 15:52:00 -0500

    add functions to user db

--- data/users.js
+++ data/users.js
@@ -3,21 +3,14 @@ const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 const uuid = require("node-uuid");
 
-async function addUser(firstName, lastName, userName, password) {
-
-//   const hash = await bcrypt.hash(password, saltRounds);
+async function addUser(userName, type, deparment) {
 
   return users().then(usersCollection => {
     let newUser = {
       _id: uuid.v4(),
-      username: userName.toLowerCase(),
-      sessionId: "",
-      hashedPassword: hash,
-      Account: {
-        firstName: firstName.toLowerCase(),
-        lastName: lastName.toLowerCase(),
-        userName: userName.toLowerCase()
-      }
+      userName: userName,
+      type: type,
+      deparment: deparment
     };
 
     return usersCollection
@@ -32,77 +25,15 @@ async function addUser(firstName, lastName, userName, password) {
   });
 }
 
-async function checkForExistingUser(username) {
+async function checkForExistingUser(userName) {
   if (!username) return false;
   return users().then(usersCollection => {
-    return usersCollection.findOne({ username: username.toLowerCase() }).then(account => {
+    return usersCollection.findOne({ userName: userName.toLowerCase() }).then(account => {
       if (!account) return false;
       return account;
     });
   });
 }
 
-async function checkForExistingUserById(userId) {
-  if (!userId) return false;
-  return users().then(usersCollection => {
-    return usersCollection.findOne({ _id: userId}).then(account => {
-      if (!account) return false;
-      return account;
-    });
-  });
-}
-
-async function validatePassword(username, password) {
-  const userInfo = await checkForExistingUser(username);
-  let compareToMatch = false
-  try {
-    // compareToMatch = await bcrypt.compare(password, String(userInfo.hashedPassword))
-  } catch (e) {
-    throw (e)
-  }
-  return compareToMatch;
-}
-
-
-async function validateSessionId(sessionId) {
-  if (!sessionId) return false;
-  return users().then(usersCollection => {
-    return usersCollection.findOne({ sessionId: sessionId }).then(account => {
-      if (!account) return false;
-      return account;
-    });
-  });
-}
-
-async function getUserInfoById(sessionId) {
-  return validateSessionId(sessionId);
-}
-
-async function addSessionId(newsessionId, username) {
-  const usersCollection = await users();
-
-
-  const updatedInfo = await usersCollection.updateOne({ username: username }, { $set: {sessionId: newsessionId}});
-
-  if (updatedInfo.modifiedCount === 0) {
-    throw "could not update session id successfully";
-  }
-
-  return await this.checkForExistingUser(username);
-}
-
-async function removeSessionId(sessionId) {
-
-  const userCollection = await users();
-
-  const updatedInfo = await userCollection.updateOne({ sessionId: sessionId }, { $set: {sessionId: null }});
-
-  if (updatedInfo.modifiedCount === 0) {
-    throw "could not update user successfully";
-  }
-
-  return await this.getUserInfoById(sessionId);
-}
-
-let exportedMethods = { addUser, checkForExistingUser, checkForExistingUserById, validatePassword, validateSessionId, getUserInfoById, addSessionId, removeSessionId }
+let exportedMethods = { addUser, checkForExistingUser }
 module.exports = exportedMethods;
